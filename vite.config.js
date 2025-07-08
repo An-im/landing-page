e19785765982728f5a99708087f88d0ca9a0fee8
Antonella Imarisio <antonellaimarisio@Antonellas-MacBook-Pro.local>
2025-07-08 11:34:23 +0100

    Fix deploy config and update layout

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/landing-page/', // exacto al nombre del repositorio
+  base: '/landing-page/', 
 });
